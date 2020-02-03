2a630dba76a65bd3788d6425528cb390ae421388
Gaven Kerr <gavenkerr@Gavens-MacBook-Pro.local>
2020-02-03 14:04:16 -0500

    Adding in the environment demo from a sepereate project

---
+++ TwistedFeathers/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Completed;
+
+public class ButtonHandler : MonoBehaviour
+{
+     //Without reference variable
+    public GameObject objectWithScript;
+
+    public void SwitchEnvironment()
+    {
+        if(objectWithScript.GetComponent<SceneShift>().scene == 0){
+            objectWithScript.GetComponent<SceneShift>().scene = 1;
+        } else {
+            objectWithScript.GetComponent<SceneShift>().scene = 0;
+        }
+
+    }
+}
