d9494723c04933436e04b59f9fbd6ab92d57d8fa
Gaven Kerr <gavenkerr@Gavens-MacBook-Pro.local>
2020-02-14 13:03:22 -0500

    working UX! woo

--- TwistedFeathers/Assets/Scripts/ButtonHandler.cs
+++ TwistedFeathers/Assets/Scripts/ButtonHandler.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Completed;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
      //Without reference variable
     public GameObject objectWithScript;
+    public GameObject ScrollView;
+    public Text myText;
+    public GameObject me;
+    private string currentText;
+    public GameObject MiniMap;
+    public GameObject MegaMap;
 
+    public void Start() {
+        currentText = myText.text;
+    }
+    
     public void SwitchEnvironment()
     {
         if(objectWithScript.GetComponent<SceneShift>().scene == 0){
@@ -16,5 +27,31 @@ public class ButtonHandler : MonoBehaviour
             objectWithScript.GetComponent<SceneShift>().scene = 0;
         }
 
+    }
+
+    public void OpenSkillSelect(){
+        if(!ScrollView.activeSelf){
+            ScrollView.SetActive(true);
+            myText.text = "Nevermind";
+            
+        } else {
+            ScrollView.SetActive(false);
+            myText.text = currentText;
+        }
+    }
+
+    
+    public void OpenForecast(){
+        if(!ScrollView.activeSelf){
+            ScrollView.SetActive(true);
+            myText.text = "Close";
+            me.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 251);
+            MiniMap.SetActive(false);
+        } else {
+            ScrollView.SetActive(false);
+            myText.text = "";
+            me.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 30);
+            MiniMap.SetActive(true);
+        }
     }
 }
---
+++ TwistedFeathers/Assets/Scripts/SceneShift 2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Completed
+{
+    public class SceneShift : MonoBehaviour
+    {
+        
+        
+        public int scene = 0;
+        public GameObject[] environments;
+        private GameObject currentEnvironment;
+        public GameObject lightSource;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentEnvironment = environments[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(scene == 0){
+                lightSource.GetComponent<Light>().color = Color.red;
+                currentEnvironment = environments[0];
+                foreach (GameObject go in GameObject.FindGameObjectsWithTag("Desert"))
+                {
+                    go.GetComponent<MeshRenderer>().enabled = true;
+                }
+
+                foreach (GameObject go in GameObject.FindGameObjectsWithTag("Swamp"))
+                {
+                    go.GetComponent<MeshRenderer>().enabled = false;
+                }
+            }
+            if(scene == 1){
+                currentEnvironment = environments[1];
+                lightSource.GetComponent<Light>().color = Color.blue;
+                foreach (GameObject go in GameObject.FindGameObjectsWithTag("Desert"))
+                {
+                    go.GetComponent<MeshRenderer>().enabled = false;
+                }
+
+                foreach (GameObject go in GameObject.FindGameObjectsWithTag("Swamp"))
+                {
+                    go.GetComponent<MeshRenderer>().enabled = true;
+                }
+            }
+        }
+    }
+}
