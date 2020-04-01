3c3a642c07c3c73f1c7ea7cff886b7febcced724
nabruins <nabruins@ncsu.edu>
2020-04-01 17:52:05 -0400

    Update GameManager.cs
    "Adam" now uses skills from skill tree rather than hard coded skills.

--- TwistedFeathers/Assets/Scripts/GameManager.cs
+++ TwistedFeathers/Assets/Scripts/GameManager.cs
@@ -67,8 +67,8 @@ public class GameManager : MonoBehaviour
             Skill_db.Add("dummy E", new Skill("Claws", "Does nothing", p_type.enemy, new List<BattleEffect>()));
             Skill_db.Add("dummy F", new Skill("Enraged", "Does nothing", p_type.enemy, new List<BattleEffect>()));
             //player skills
-            Skill_db.Add("FeatherDagger", new Skill("Feather Dagger", "Deals 20 damage", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.damage, 20f, "Feather Dagger") }));
-            Skill_db.Add("Sabotage", new Skill("Sabotage", "Reduce enemy defense by 25% for 1 turn", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.buff, -.25f, 3, "defense") }));
+            //Skill_db.Add("FeatherDagger", new Skill("Feather Dagger", "Deals 20 damage", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.damage, 20f, "Feather Dagger") }));
+            //Skill_db.Add("Sabotage", new Skill("Sabotage", "Reduce enemy defense by 25% for 1 turn", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.buff, -.25f, 3, "defense") }));
             Skill_db.Add("DefensiveFeathers", new Skill("Defensive Feathers", "Increase defense by 10%", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.buff, .1f, 3, "defense") }));
             Skill_db.Add("smarty A", new Skill("Dagger", "Does nothing", p_type.player, new List<BattleEffect>()));
             Skill_db.Add("smarty B", new Skill("Pollen Bombs", "Does nothing", p_type.player, new List<BattleEffect>()));
@@ -93,10 +93,11 @@ public class GameManager : MonoBehaviour
             Skill_db.Add("Ben's Skill", new Skill("Ben's Skill", "Deals 10 damage", p_type.player, new List<BattleEffect>() { new BattleEffect(e_type.damage, 10f, "This is B smarty") }));
 
             Player_db.Add("person A", new Player("Adam"));
-            Player_db["person A"].AddSkill(Skill_db["Adam's Skill"]);
-            Player_db["person A"].AddSkill(Skill_db["Sabotage"]);
-            Player_db["person A"].AddSkill(Skill_db["DefensiveFeathers"]);
-            Player_db["person A"].AddSkill(Skill_db["FeatherDagger"]);
+            Player_db["person A"].LoadSkillTree("Assets/Scripts/SkillEditor/Data/Test.json");
+            //Player_db["person A"].AddSkill(Skill_db["Adam's Skill"]);
+            //Player_db["person A"].AddSkill(Skill_db["Sabotage"]);
+            //Player_db["person A"].AddSkill(Skill_db["DefensiveFeathers"]);
+            //Player_db["person A"].AddSkill(Skill_db["FeatherDagger"]);
             Player_db["person A"].myPrefab = playerPrefab;
             Player_db.Add("person B", new Player("Ben"));
             Player_db["person B"].AddSkill(Skill_db["Ben's Skill"]);
