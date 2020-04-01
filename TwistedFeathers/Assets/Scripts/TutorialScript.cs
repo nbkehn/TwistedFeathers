00c7bd4616be5f8c3453191a5f85c990d3412665
Gaven Kerr <gavenkerr@gavens-mbp.attlocal.net>
2020-03-31 23:24:45 -0400

    tutorial works

---
+++ TwistedFeathers/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    private int segment = 0;
+    public GameObject tutorialCanvas;
+    public Animator animator;
+
+    public void StartTutorial(){
+        segment = 1;
+        animator.Play("HealthBars");
+    }
+
+    public void CancelTutorial(){
+        tutorialCanvas.SetActive(false);
+    }
+
+    public void next(){      
+        switch (segment)
+        {
+            case 1:
+                segment = 2;
+                animator.Play("miniMap");
+                break;
+            case 2:
+                animator.Play("TakeTurn");
+                segment = 3;
+                break;
+            case 3:
+                animator.Play("Close");
+                break;
+            default:
+                Debug.Log("InvalidAnimationState");
+                break;
+        }
+    }
+}
